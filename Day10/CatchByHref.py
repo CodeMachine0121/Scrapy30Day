1965d8dbc4a7f90c28f615861a31e26d1dacaca4
CodeMachine <CodeMachine0121@gmail.com>
2019-12-06 02:27:39 +0800

    Change page

---
+++ Day10/CatchByHref.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+#page2 url = https://ithelp.ithome.com.tw/articles?tab=tech&page=2
+for page in range(1,11):
+    titles=[]
+    url='https://ithelp.ithome.com.tw/articles?tab=tech&page='+str(page)
+    r = requests.get(url).text
+    soup = BeautifulSoup(r,'lxml')
+
+    print(url)
+    articles = soup.find_all('div',class_="qa-list")
+    if len(articles)==0:
+        print("沒有文章了@@")
+        break
+    for article in articles:
+        title  = article.find("a",class_="qa-list__title-link")
+        titles.append(title.text)
+  
+    print("Page: ",page)
+    print("Titles:",titles)
+    print("==================================================")
